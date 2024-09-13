447ef0f49423b9f61b2761ee8bdde5c857971094
Shreya <shreyaprakashan924@gmail.com>
2024-09-13 15:01:55 +0530

    pseudo

--- main/java/com/example/spring/project/ems/D.java
+++ main/java/com/example/spring/project/ems/D.java
@@ -3,5 +3,6 @@ package com.example.spring.project.ems;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 public interface D {
+    //System
     void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception;
 }
